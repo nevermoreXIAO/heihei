ff611d1dfd5336e2d3ed41417e7b0a90e6ba5af6
LKKKKKK <31401131@stu.edu.cn>
2017-06-13 22:16:34 +0800

    test

--- heihei.js
+++ heihei.js
@@ -1,4 +1,25 @@
 /**
  * Created by Administrator on 2017/6/13 0013.
  */
-console.log(10);
+var game={
+    data:[],//单元格中的所有数字
+    score:0,
+    state:1,
+    RUNNING:1,
+    GAME_OVER:0,
+    PLAYING:2,//动画正在播放中
+    start:function(){//启动游戏时调用
+        this.data=[ [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0] ];
+        //在两个随机位置生成2或4
+        this.score=0;
+        this.state=this.RUNNING;
+        var div=document.getElementById("gameOver");
+        div.style.display="none";
+        this.randomNum();
+        this.randomNum();
+        //document.write(this.data.join("<br/>"));
+        this.updateView();
+    }
